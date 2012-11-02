0782e24b159b79b1889089ad818e12e789c23a40
Marcel Bohländer <marcel.bohlaender@cn-consult.eu>
2012-11-02 14:03:39 +0100

    Add architecture-description and first server parts.

---
+++ server/index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+*
+* @author < >
+* @version 
+* @module
+*/
+
+/**
+*
+*/
+public class util
+{
+   public function __construct()
+   {
+   
+   }
+   
+   public function validateJson($_json)
+   {
+      return true;
+   }
+}
+?>
