fb85b49b6f7ac36f4ecff721e9da2f3c94e94055
Flennic <flennic@googlemail.com>
2012-12-17 16:17:41 +0100

    Add First Layout
    Application doesn't run properly. Crashes with an error message
    "Unfortunely, Pam has stopped". Look LogCat for further information.

--- gen/com/example/hello/world/R.java
+++ gen/com/example/hello/world/R.java
@@ -15,11 +15,23 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070001;
-        public static final int webView1=0x7f070000;
+        public static final int Edit=0x7f07000a;
+        public static final int Exit=0x7f07000b;
+        public static final int LinearLayout2=0x7f070005;
+        public static final int RelativeLayout1=0x7f070000;
+        public static final int View=0x7f070009;
+        public static final int button1=0x7f070002;
+        public static final int button2=0x7f070004;
+        public static final int editText1=0x7f070006;
+        public static final int editText2=0x7f070007;
+        public static final int menu_settings=0x7f07000c;
+        public static final int textView1=0x7f070008;
+        public static final int textView2=0x7f070003;
+        public static final int textView3=0x7f070001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int view_screen=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
