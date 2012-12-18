39848b9ef3ec0913e2c8e6a0f4e565becbade743
Flennic <flennic@googlemail.com>
2012-12-18 16:34:11 +0100

    Add Class(es) for Tabs
    Adjusted some files to fit better to the project. Documention files are
    now in the folder 'documentation'.

--- gen/com/example/hello/world/R.java
+++ gen/com/example/hello/world/R.java
@@ -32,6 +32,9 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int view_screen=0x7f030001;
+        public static final int view_screen_edit=0x7f030002;
+        public static final int view_screen_exit=0x7f030003;
+        public static final int view_screen_view=0x7f030004;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
---
+++ src/com/example/hello/world/TabWidgetViewActivity.java
@@ -0,0 +1,16 @@
+package com.example.hello.world;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TabWidgetViewActivity extends Activity {
+	
+	public void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.view_screen_view);
+		
+	}
+
+}
