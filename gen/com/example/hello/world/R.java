63965313c3e06cf1acc8745a6dac8884a84212a0
Flennic <flennic@googlemail.com>
2013-01-14 18:19:09 +0100

    Add Variables
    Added last variables.

--- gen/com/example/hello/world/R.java
+++ gen/com/example/hello/world/R.java
@@ -8,6 +8,12 @@
 package com.example.hello.world;
 
 public final class R {
+    public static final class array {
+        public static final int Edit_screen2=0x7f040002;
+        public static final int Login=0x7f040000;
+        public static final int View_screen1=0x7f040001;
+        public static final int Viewsceen=0x7f040003;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -22,21 +28,21 @@ public final class R {
         public static final int ic_tab_widget_exit=0x7f020008;
     }
     public static final class id {
-        public static final int LinearLayout2=0x7f070004;
-        public static final int RelativeLayout1=0x7f070000;
-        public static final int button1=0x7f070008;
-        public static final int button2=0x7f070005;
-        public static final int button3=0x7f07000b;
-        public static final int button4=0x7f07000d;
-        public static final int editText1=0x7f070006;
-        public static final int editText2=0x7f07000a;
-        public static final int exit=0x7f070009;
-        public static final int login=0x7f070002;
-        public static final int menu_settings=0x7f07000e;
-        public static final int spinner1=0x7f07000c;
-        public static final int textView1=0x7f070007;
-        public static final int textView2=0x7f070003;
-        public static final int textView3=0x7f070001;
+        public static final int LinearLayout2=0x7f080004;
+        public static final int RelativeLayout1=0x7f080000;
+        public static final int button1=0x7f080008;
+        public static final int button2=0x7f080005;
+        public static final int button3=0x7f08000b;
+        public static final int button4=0x7f08000d;
+        public static final int editText1=0x7f080006;
+        public static final int editText2=0x7f08000a;
+        public static final int exit=0x7f080009;
+        public static final int login=0x7f080002;
+        public static final int menu_settings=0x7f08000e;
+        public static final int spinner1=0x7f08000c;
+        public static final int textView1=0x7f080007;
+        public static final int textView2=0x7f080003;
+        public static final int textView3=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -46,15 +52,19 @@ public final class R {
         public static final int view_screen=0x7f030004;
     }
     public static final class menu {
-        public static final int activity_main=0x7f060000;
+        public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int Applist=0x7f050004;
+        public static final int Change_Note=0x7f050005;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int menu_settings=0x7f050002;
+        public static final int password=0x7f050006;
+        public static final int title_activity_main=0x7f050003;
+        public static final int username=0x7f050007;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }
