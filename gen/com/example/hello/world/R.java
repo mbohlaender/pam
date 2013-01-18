a29d73111a067e8928ae5db2ec75aa273220fc5f
Flennic <flennic@googlemail.com>
2013-01-18 21:36:06 +0100

    Modify
    Layout

--- gen/com/example/hello/world/R.java
+++ gen/com/example/hello/world/R.java
@@ -23,9 +23,6 @@ public final class R {
         public static final int ic_action_tabwidgetlogout=0x7f020003;
         public static final int ic_action_tabwidgetlogout_1=0x7f020004;
         public static final int ic_launcher=0x7f020005;
-        public static final int ic_tab_view=0x7f020006;
-        public static final int ic_tab_widget_edit=0x7f020007;
-        public static final int ic_tab_widget_exit=0x7f020008;
     }
     public static final class id {
         public static final int LinearLayout2=0x7f080004;
@@ -55,11 +52,11 @@ public final class R {
         public static final int activity_main=0x7f070000;
     }
     public static final class string {
-        public static final int Applist=0x7f050004;
-        public static final int Change_Note=0x7f050005;
         public static final int app_name=0x7f050000;
+        public static final int change_note=0x7f050005;
         public static final int hello_world=0x7f050001;
         public static final int menu_settings=0x7f050002;
+        public static final int notelist=0x7f050004;
         public static final int password=0x7f050006;
         public static final int title_activity_main=0x7f050003;
         public static final int username=0x7f050007;
